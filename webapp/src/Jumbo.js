8152f1fda75c1466099268dcfc9055998bb9c2ad
chidi150c <chidi150c@gmail.com>
2023-07-04 15:23:13 +0100

    testing git workflow

--- webapp/src/Jumbo.js
+++ webapp/src/Jumbo.js
@@ -10,7 +10,7 @@ function Jumbo() {
           <div className="w3-display-left w3-text-white" style={{padding:'48px'}}>
             <span className="w3-jumbo w3-hide-small">Delivering innovative solutions powered by cutting-edge technologies.</span><br />
             <span className="w3-xxlarge w3-hide-large w3-hide-medium">Delivering resourceful solutions</span><br />
-            <span className="w3-large">Cutting-edge technology solutions delivered resourcefully.</span>
+            <span className="w3-large">Cutting-edge technology technology solutions delivered resourcefully.</span>
             <p><a href="#about" className="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Learn more and start today</a></p>
           </div> 
         </div>    
