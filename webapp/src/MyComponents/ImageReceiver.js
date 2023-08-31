33339378c2950cd1a47d0d487c4015a5d99806db
chidi150c <chidi150c@gmail.com>
2023-08-31 05:56:57 +0100

    moving on ooh1

--- webapp/src/MyComponents/ImageReceiver.js
+++ webapp/src/MyComponents/ImageReceiver.js
@@ -99,8 +99,45 @@ class ImageReceiver extends Component {
               </tr>
             </table>
           </div>
-          <div className="w3-quarter" style={{padding: '35px 10px'}}>
-            <img src={trading2} width="100%" height={"410px"} alt="Trading Chart" />            
+          <div className="w3-quarter">
+            <h2 style={{color: 'white'}}>Feeds</h2>
+            <table className="w3-table w3-striped w3-white">
+              <tr>
+                <td><i className="fa fa-user w3-text-blue w3-large"></i></td>
+                <td>New record, over 90 views.</td>
+                <td><i>10 mins</i></td>
+              </tr>
+              <tr>
+                <td><i className="fa fa-bell w3-text-red w3-large"></i></td>
+                <td>Database error.</td>
+                <td><i>15 mins</i></td>
+              </tr>
+              <tr>
+                <td><i className="fa fa-users w3-text-yellow w3-large"></i></td>
+                <td>New record, over 40 users.</td>
+                <td><i>17 mins</i></td>
+              </tr>
+              <tr>
+                <td><i className="fa fa-comment w3-text-red w3-large"></i></td>
+                <td>New comments.</td>
+                <td><i>25 mins</i></td>
+              </tr>
+              <tr>
+                <td><i className="fa fa-bookmark w3-text-blue w3-large"></i></td>
+                <td>Check transactions.</td>
+                <td><i>28 mins</i></td>
+              </tr>
+              <tr>
+                <td><i className="fa fa-laptop w3-text-red w3-large"></i></td>
+                <td>CPU overload.</td>
+                <td><i>35 mins</i></td>
+              </tr>
+              <tr>
+                <td><i className="fa fa-share-alt w3-text-green w3-large"></i></td>
+                <td>New shares.</td>
+                <td><i>39 mins</i></td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
